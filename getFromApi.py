3355a48cdcfa6b5d6e14c7a7e4a116a10f9a2a68
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-12-07 15:00:05 -0600

    adds records fuel type from 2nd endpoint

--- getFromApi.py
+++ getFromApi.py
@@ -18,19 +18,13 @@ illinoisPowerPlants = requests.get(
 
 
 for i in range(3):
-    print(i)
-    print(illinoisPowerPlants['category']['childcategories'][i]['name'])
     plantCategoryID = illinoisPowerPlants['category']['childcategories'][i]['category_id']
-    powerPlants["powerPlants"].insert(i, {})
-    powerPlants["powerPlants"][i]["category_id"] = illinoisPowerPlants['category']['childcategories'][i]['category_id']
-    powerPlants["powerPlants"][i]["name"] = illinoisPowerPlants['category']['childcategories'][i]['name']
 
     plantNameAndSeries = requests.get(
         "https://api.eia.gov/category/?api_key=1d97ccab56e4ea052f94379e9adb787a&category_id=" + str(plantCategoryID)).json()
 
     for j in range(len(plantNameAndSeries['category']['childseries'])):
         if(plantNameAndSeries['category']['childseries'][j]['name'].__contains__("Net generation")):
-            print(plantNameAndSeries['category']['childseries'][j]['name'])
             plantSeriesID = plantNameAndSeries['category']['childseries'][j]['series_id']
             break
 
@@ -38,8 +32,43 @@ for i in range(3):
     plantInfo = requests.get(
         "https://api.eia.gov/series/?api_key=1d97ccab56e4ea052f94379e9adb787a&series_id=" + plantSeriesID).json()
 
+    for k in range(len(plantNameAndSeries['category']['childseries'])):
+        if(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("natural gas")):
+            stationType = "petroleum"
+            break
+        elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("coal")):
+            stationType = "petroleum"
+            break
+        elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("oil")):
+            stationType = "petroleum"
+            break
+        elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("nuclear")):
+            stationType = "nuclear"
+            break
+        elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("hydroelectric")):
+            stationType = "hydroelectric"
+            break
+        elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("wind")):
+            stationType = "wind"
+            break
+        elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("solar")):
+            stationType = "solar"
+            break
+        else:
+            stationType = "other"
+
+    # create powerplant entry and populate with info
+    powerPlants["powerPlants"].insert(i, {})
+    powerPlants["powerPlants"][i]["name"] = illinoisPowerPlants['category']['childcategories'][i]['name']
+    powerPlants["powerPlants"][i]["coordinates"] = plantInfo['series'][0]['latlon']
+    powerPlants["powerPlants"][i]["2020 MWH output"] = plantInfo['series'][0]['data'][0][1]
+    powerPlants["powerPlants"][i]["type"] = stationType
+
+    print(i)
+    print(illinoisPowerPlants['category']['childcategories'][i]['name'])
     print("coordinates: " + plantInfo['series'][0]['latlon'])
     print("2020 output: " + str(plantInfo['series'][0]['data'][0][1]))
+    print(stationType)
 
 
 with open("powerplants.json", "w") as outfile:
