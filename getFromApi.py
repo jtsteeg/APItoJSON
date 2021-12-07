e4b0c3ccf8579c7cc7ac93a3e2d9509189d262fa
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-12-07 11:24:17 -0600

    adds get call to 2nd endpoint to collect plant series_id

--- getFromApi.py
+++ getFromApi.py
@@ -14,17 +14,24 @@ illinoisPowerPlants = requests.get(
 
 # print(illinoisPowerPlants['category']['childcategories'])
 
+# len(illinoisPowerPlants['category']['childcategories']))
 
-for i in range(len(illinoisPowerPlants['category']['childcategories'])):
+
+for i in range(3):
     print(i)
     print(illinoisPowerPlants['category']['childcategories'][i]['name'])
+    plantCategoryID = illinoisPowerPlants['category']['childcategories'][i]['category_id']
     powerPlants["powerPlants"].insert(i, {})
     powerPlants["powerPlants"][i]["category_id"] = illinoisPowerPlants['category']['childcategories'][i]['category_id']
     powerPlants["powerPlants"][i]["name"] = illinoisPowerPlants['category']['childcategories'][i]['name']
-    # powerplants[i] =
-    # powerplants[i]["name"]
-    # print(i['name'])
-    # print(i['category_id'])
+
+    plantNameAndSeries = requests.get(
+        "https://api.eia.gov/category/?api_key=1d97ccab56e4ea052f94379e9adb787a&category_id=" + str(plantCategoryID)).json()
+
+    plantSeriesID = plantNameAndSeries['category']['childseries'][0]['series_id']
+
+    print(plantSeriesID)
+
 
 with open("powerplants.json", "w") as outfile:
     json.dump(powerPlants, outfile)
