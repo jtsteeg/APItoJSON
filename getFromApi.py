d387906620ec718c2457323474f78200d099c4e7
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-12-07 15:18:07 -0600

    calculates total power output of all sites, adds field to JSON

--- getFromApi.py
+++ getFromApi.py
@@ -3,6 +3,7 @@ import json
 import requests
 
 powerPlants = {
+    "totalOutput": 0,
     "powerPlants": []
 }
 
@@ -16,8 +17,10 @@ illinoisPowerPlants = requests.get(
 
 # len(illinoisPowerPlants['category']['childcategories']))
 
+print("number of power stations: " +
+      str(len(illinoisPowerPlants['category']['childcategories'])))
 
-for i in range(3):
+for i in range(5):
     plantCategoryID = illinoisPowerPlants['category']['childcategories'][i]['category_id']
 
     plantNameAndSeries = requests.get(
@@ -28,7 +31,7 @@ for i in range(3):
             plantSeriesID = plantNameAndSeries['category']['childseries'][j]['series_id']
             break
 
-    print(plantSeriesID)
+    # print(plantSeriesID)
     plantInfo = requests.get(
         "https://api.eia.gov/series/?api_key=1d97ccab56e4ea052f94379e9adb787a&series_id=" + plantSeriesID).json()
 
@@ -63,12 +66,14 @@ for i in range(3):
     powerPlants["powerPlants"][i]["coordinates"] = plantInfo['series'][0]['latlon']
     powerPlants["powerPlants"][i]["2020 MWH output"] = plantInfo['series'][0]['data'][0][1]
     powerPlants["powerPlants"][i]["type"] = stationType
+    powerPlants["totalOutput"] = powerPlants["totalOutput"] + \
+        plantInfo['series'][0]['data'][0][1]
 
     print(i)
     print(illinoisPowerPlants['category']['childcategories'][i]['name'])
     print("coordinates: " + plantInfo['series'][0]['latlon'])
     print("2020 output: " + str(plantInfo['series'][0]['data'][0][1]))
-    print(stationType)
+    print("fuel type: " + stationType)
 
 
 with open("powerplants.json", "w") as outfile:
