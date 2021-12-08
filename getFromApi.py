507f646029043539e8eff03f7b5cc17df6561669
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-12-07 21:25:25 -0600

    utilizes enviroment variables to access API

--- getFromApi.py
+++ getFromApi.py
@@ -1,48 +1,47 @@
 import json
-
 import requests
+import os
+
+EIA_KEY = os.getenv('EIA_KEY')
+
 
 powerPlants = {
     "totalOutput": 0,
     "powerPlants": []
 }
+eiaUri = "https://api.eia.gov/category/?api_key=%s" % EIA_KEY
 
 
-illinoisPowerPlants = requests.get(
+# 1st endpoint
+illinoisPowerPlants = requests.get(eiaUri + "&category_id=902944").json()
 
-    "https://api.eia.gov/category/?api_key=1d97ccab56e4ea052f94379e9adb787a&category_id=902944").json()
-
-
-# print(illinoisPowerPlants['category']['childcategories'])
-
-# len(illinoisPowerPlants['category']['childcategories']))
 currentPlants = 0
-
 for i in range(len(illinoisPowerPlants['category']['childcategories'])):
 
     plantCategoryID = illinoisPowerPlants['category']['childcategories'][i]['category_id']
 
+    # 2nd endpoint
     plantNameAndSeries = requests.get(
-        "https://api.eia.gov/category/?api_key=1d97ccab56e4ea052f94379e9adb787a&category_id=" + str(plantCategoryID)).json()
+        eiaUri + "&category_id=" + str(plantCategoryID)).json()
 
     for j in range(len(plantNameAndSeries['category']['childseries'])):
         if(plantNameAndSeries['category']['childseries'][j]['name'].__contains__("Net generation")):
             plantSeriesID = plantNameAndSeries['category']['childseries'][j]['series_id']
             break
 
-    # print(plantSeriesID)
+    # 3rd endpoint
     plantInfo = requests.get(
-        "https://api.eia.gov/series/?api_key=1d97ccab56e4ea052f94379e9adb787a&series_id=" + plantSeriesID).json()
+        eiaUri + "&series_id=" + plantSeriesID).json()
 
     for k in range(len(plantNameAndSeries['category']['childseries'])):
         if(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("natural gas")):
-            stationType = "petroleum"
+            stationType = "natural gas"
             break
         elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("coal")):
-            stationType = "petroleum"
+            stationType = "coal"
             break
         elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("oil")):
-            stationType = "petroleum"
+            stationType = "oil"
             break
         elif(plantNameAndSeries['category']['childseries'][k]['name'].__contains__("nuclear")):
             stationType = "nuclear"
@@ -59,7 +58,7 @@ for i in range(len(illinoisPowerPlants['category']['childcategories'])):
         else:
             stationType = "other"
 
-    # create powerplant entry and populate with info
+    # create a new entry in powerPlants and populate it with info from endpoints
     if(plantInfo['series'][0]['data'][0][0] == "2020" and plantInfo['series'][0]['data'][0][1] > 0):
         print(i)
         print(illinoisPowerPlants['category']['childcategories'][i]['name'])
@@ -75,23 +74,6 @@ for i in range(len(illinoisPowerPlants['category']['childcategories'])):
         powerPlants["totalOutput"] = powerPlants["totalOutput"] + \
             plantInfo['series'][0]['data'][0][1]
         currentPlants += 1
-
-        # powerPlants["powerPlants"].insert(i, {})
-        # powerPlants["powerPlants"][i]["name"] = illinoisPowerPlants['category']['childcategories'][i]['name']
-        # powerPlants["powerPlants"][i]["coordinates"] = plantInfo['series'][0]['latlon']
-        # powerPlants["powerPlants"][i]["2020 MWH output"] = plantInfo['series'][0]['data'][0][1]
-        # powerPlants["powerPlants"][i]["type"] = stationType
-        # powerPlants["totalOutput"] = powerPlants["totalOutput"] + \
-        #     plantInfo['series'][0]['data'][0][1]
-
-        # print(i)
-        # print(illinoisPowerPlants['category']['childcategories'][i]['name'])
-        # print("coordinates: " + plantInfo['series'][0]['latlon'])
-        # print("2020 output: " + str(plantInfo['series'][0]['data'][0][1]))
-        # print("fuel type: " + stationType)
-
-print("number of power stations: " +
-      str(len(illinoisPowerPlants['category']['childcategories'])))
 
 with open("powerplants.json", "w") as outfile:
     json.dump(powerPlants, outfile)
